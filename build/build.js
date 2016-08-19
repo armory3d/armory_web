54b1f6385df5afde701bf12a2010d9be6360b253
Lubos Lenco <lubos.lenco@gmail.com>
2016-08-19 12:29:13 +0200

    Sync.

--- build/build.js
+++ build/build.js
@@ -1,9 +1,9 @@
-var fs = require('fs');
+let fs = require('fs');
 
-var writeHtml = function(fileName, title) {
-	var header = fs.readFileSync('header.html', 'utf8').replace('ID_TITLE', title);
-	var footer = fs.readFileSync('footer.html', 'utf8');
-	var html = fs.readFileSync(fileName, 'utf8');
+let writeHtml = function(fileName, title) {
+	let header = fs.readFileSync('header.html', 'utf8').replace('ID_TITLE', title);
+	let footer = fs.readFileSync('footer.html', 'utf8');
+	let html = fs.readFileSync(fileName, 'utf8');
 	fs.writeFileSync('../' + fileName, header + html + footer);
 }
 
@@ -11,3 +11,4 @@ writeHtml('index.html', 'Armory');
 writeHtml('news.html', 'News');
 writeHtml('gallery.html', 'Gallery');
 writeHtml('buy.html', 'Buy');
+writeHtml('support.html', 'Support');
--- build/manual/build.js
+++ build/manual/build.js
@@ -1,26 +1,28 @@
-var fs = require('fs-extra');
-var glob = require("glob");
-var showdown = require('./showdown.js');
-var converter = new showdown.Converter();
+let fs = require('fs-extra');
+let glob = require("glob");
+let showdown = require('./showdown.js');
+let converter = new showdown.Converter();
 
-var footer = fs.readFileSync('../footer.html', 'utf8');
-var header_manual = fs.readFileSync('../header_manual.html', 'utf8');
-var footer_manual = fs.readFileSync('../footer_manual.html', 'utf8');
+let footer = fs.readFileSync('../footer.html', 'utf8');
+let header_manual = fs.readFileSync('../header_manual.html', 'utf8');
+let footer_manual = fs.readFileSync('../footer_manual.html', 'utf8');
 
-var mdToHtml = function(basename, title) {
-	var header = fs.readFileSync('../header.html', 'utf8').replace('ID_TITLE', title);
-	var data = fs.readFileSync(basename + '.md', 'utf8');
-	var html = converter.makeHtml(data).replace(/.md"/g, '.html"');
-	fs.writeFileSync(basename + '.html', header + header_manual + html + footer_manual + footer);
+let mdToHtml = function(sourcename, targetname, title) {
+	let header = fs.readFileSync('../header.html', 'utf8').replace('ID_TITLE', title);
+	let data = fs.readFileSync(sourcename + '.md', 'utf8');
+	let html = converter.makeHtml(data).replace(/.md"/g, '.html"');
+	fs.writeFileSync(targetname + '.html', header + header_manual + html + footer_manual + footer);
 }
 
 fs.removeSync('../../manual');
 fs.copySync('../../../armory_docs', '../../manual');
 
 glob("../../manual/**/*.md", null, function (er, files) {
-	for (var i = 0; i < files.length; i++) {
-		var file = files[i];
-		var basename = file.substr(0, file.lastIndexOf('.')) || file;
-		mdToHtml(basename, 'Manual');
+	for (let i = 0; i < files.length; i++) {
+		let file = files[i];
+		let basename = file.substr(0, file.lastIndexOf('.')) || file;
+		let targetname = basename;
+		if (targetname === '../../manual/readme') targetname = '../../manual/index';
+		mdToHtml(basename, targetname, 'Manual');
 	}
 });
