a7488dfba8123171bc4ca5d1ce3fc8d29404a33b
luboslenco <lubos.lenco@gmail.com>
2018-07-05 17:35:52 +0200

    Sync new web

--- build/make.js
+++ build/make.js
@@ -1,13 +1,14 @@
 let fs = require('fs');
 
-let writeHtml = function(fileName, title) {
-	let header = fs.readFileSync('header.html', 'utf8').replace('ID_TITLE', title);
+let writeHtml = function(fileName) {
+	let header = fs.readFileSync('header.html', 'utf8');
 	let footer = fs.readFileSync('footer.html', 'utf8');
 	let html = fs.readFileSync(fileName, 'utf8');
 	fs.writeFileSync('../' + fileName, header + html + footer);
 }
 
-writeHtml('index.html', 'Armory');
-writeHtml('news.html', 'News');
-writeHtml('community.html', 'Community');
-writeHtml('download.html', 'Download');
+writeHtml('index.html');
+writeHtml('news.html');
+writeHtml('community.html');
+writeHtml('download.html');
+writeHtml('fund.html');
