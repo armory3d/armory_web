58b275416bee6e47985ee8fbe8b3560838d2a306
rpaladin <69180012+rpaladin@users.noreply.github.com>
2022-03-18 12:09:23 -0700

    Implement rename for build folder

---
+++ templates/make.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+
+let writeHTML = function(fileName) {
+	let header = fs.readFileSync('header.html', 'utf8');
+	let footer = fs.readFileSync('footer.html', 'utf8');
+	let html = fs.readFileSync(fileName, 'utf8');
+	fs.writeFileSync('../' + fileName, header + html + footer);
+}
+
+writeHTML('404.html');
+writeHTML('index.html');
+writeHTML('news.html');
+writeHTML('community.html');
+writeHTML('download.html');
+writeHTML('fund.html');
+writeHTML('notes.html');
