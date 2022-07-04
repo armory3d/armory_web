2a20532310f4492836c7740ac66c5ec047a9d40f
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-07-04 16:27:01 -0700

    Deploy

--- templates/make.js
+++ templates/make.js
@@ -1,16 +1,14 @@
-let fs = require('fs');
-
+let fs = require("fs");
 let writeHTML = function(fileName) {
-	let header = fs.readFileSync('header.html', 'utf8');
-	let footer = fs.readFileSync('footer.html', 'utf8');
-	let html = fs.readFileSync(fileName, 'utf8');
-	fs.writeFileSync('../' + fileName, header + html + footer);
+	let header = fs.readFileSync("header.html", "utf8");
+	let footer = fs.readFileSync("footer.html", "utf8");
+	let html = fs.readFileSync(fileName, "utf8");
+	fs.writeFileSync("../" + fileName, header + "\n" + html + "\n" + footer);
 };
-
-writeHTML('404.html');
-writeHTML('index.html');
-writeHTML('news.html');
-writeHTML('notes.html');
-writeHTML('features.html');
-writeHTML('community.html');
-writeHTML('download.html');
+writeHTML("404.html");
+writeHTML("index.html");
+writeHTML("news.html");
+writeHTML("notes.html");
+writeHTML("features.html");
+writeHTML("community.html");
+writeHTML("download.html");
