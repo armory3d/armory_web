8028c46f33f59a11f2ecfe67283a126b1a64b74d
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-08-15 13:35:04 -0700

    Boostrap Migratoin #1

--- templates/make.js
+++ templates/make.js
@@ -5,10 +5,4 @@ let writeHTML = function(fileName) {
 	let html = fs.readFileSync(fileName, "utf8");
 	fs.writeFileSync("../" + fileName, header + "\n" + html + "\n" + footer);
 };
-writeHTML("404.html");
 writeHTML("index.html");
-writeHTML("news.html");
-writeHTML("notes.html");
-writeHTML("features.html");
-writeHTML("community.html");
-writeHTML("download.html");
