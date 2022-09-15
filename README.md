fe1937b2f219b1a10c41b43e0f1ce3d9fdefdceb
RPaladin <69180012+rpaladin@users.noreply.github.com>
2022-09-15 11:56:01 -0700

    Implement proposed changes

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Development - Intro
 * The [Armory3D](https://armory3d.org) website is hosted on [Github Pages](https://pages.github.com).
 * [`make.js`](https://github.com/armory3d/armory_web/blob/main/make.js) is a [Node.js](https://nodejs.org/) script that:
-  * Builds webpages in [`/templates/`](https://github.com/armory3d/armory_web/tree/main/templates) and its sub-directories, both on init and when new changes are detected.
+  * Compiles webpages in [`/templates/`](https://github.com/armory3d/armory_web/tree/main/templates) and its sub-directories, both on init and when new changes are detected.
+    * **Warning:** For Linux users, new changes are _not_ automatically detected for files in sub-folders. This is a Node.js limitation, officially stated [here](https://nodejs.org/docs/latest/api/fs.html#fs_caveats).
+  * Compiling of release notes can slow due to the large amount of changelog files needed to be compiled. If you don't need to compile release notes, change the `COMPILE_NOTES` variable in `make.js` to `false` to disable compiling of release notes. Don't forget to re-enable this variable when you're modifying existing release notes or creating new ones, otherwise changes won't be compiled.
+  * Changes to the `make.js` script itself require any active instances of `make.js` terminals to be closed and restarted in order for changes to be updated.
   * Serves webpages via HTTP on port `80` (feel free to change it to a different port).
   * Sends 302 redirect if you attempt to access a webpage without inserting its `.html` extension.
   * Sends 404 if error or missing content occurs.
+  * Standard for image & video formats:
+    * Original formats for images should be `JPEG` or `PNG`.
+    * Original formats for video should be `GIF` or `MP4`.
+    * Conversion format should be `WebP` format for optimal web experiences.
+    * Standard resolution for both images & videos should be of 1920x1080 screen resolution in original form for best quality before compression.
+    * Convert images & videos to WebP format conversion using [ezgif](https://ezgif.com), a nifty online tool.
+      * Using this tool, there should only be 4 steps - this may change in the future depending on the service.
+        * **Step 1:** Choose the correct conversion process e.g. PNG -> WebP or GIF -> WebP.
+          * **Warning:** Make sure you really choose the correct conversion type! Otherwise it won't work.
+        * **Step 2:** Upload or state URL path to image or video.
+        * **Step 3:** Resize your image or video using provided `Resize` service tools that should appear after format conversion.
+        * **Step 4:** Set resize type to `Stretch to fit` to maintain original aspect ratio.
 # Development - Setup
 * The Armory SDK ([armsdk](https://github.com/armory3d/armsdk)) ships with Node.js: no need to download it seperately.<br />
   * The Node.js executables can be found in your `armsdk/nodejs` directory.<br />
@@ -15,8 +30,8 @@
 ```bash
 @echo off
 SET PATH=node;"D:\Blender\armsdk\nodejs"
-SET MAKE_DIR=D:\Websites\armory_web
-SET MAKE_SCRIPT=D:\Websites\armory_web\make.js
+SET MAKE_DIR="D:\Websites\armory_web"
+SET MAKE_SCRIPT="D:\Websites\armory_web\make.js"
 cd %MAKE_DIR%
 node %MAKE_SCRIPT%
 ```
@@ -25,22 +40,22 @@ node %MAKE_SCRIPT%
 ```bash
 @set +v
 EXPORT PATH="$node:D:\Blender\armsdk\nodejs"
-EXPORT MAKE_DIR=D:\Websites\armory_web
-EXPORT MAKE_SCRIPT=D:\Websites\armory_web\make.js
+EXPORT MAKE_DIR="D:\Websites\armory_web"
+EXPORT MAKE_SCRIPT="D:\Websites\armory_web\make.js"
 cd $MAKE_DIR
 node $MAKE_SCRIPT
 ```
 # Development - Process
-## Build - Main : Intro
+## Compile - Main : Intro
 * HTML files that reside in the parent, [`/armory_web/`](https://github.com/armory3d/armory_web) root directory are webpages meant to be hosted on Github Pages.
   * _**DO NOT**_ edit these HTML files.
     * Instead, modify their component counterparts that reside in the `/armory_web/templates` directory.
       * These webpage components are later on dynamically injected into their main counterparts using `make.js` and `Node.js`.
-## Build - Main : Header + %Main Content% + Footer
+## Compile - Main : Header + %Main Content% + Footer
 * Each webpage in the parent, `/armory_web/` root directory; except `header.html`, `footer.html` & `notes.html` is comprised of 3 separate webpage sections:
   * `header.html`, `%MAIN_CONTENT%`, `footer.html`.
     * `%MAIN_CONTENT%` can be any component, such as `community.html` or `download.html`.
-### Build - Examples:
+### Compile - Examples:
 **How to update Header content**
 * Make changes to `/armory_web/templates/header.html`.
 * Save.
@@ -56,7 +71,7 @@ node $MAKE_SCRIPT
 * Save.
 * Run `make.js` if you haven't already.
 * Refresh your Armory website page if you have it open.
-## Build - Notes - Intro
+## Compile - Notes - Intro
 * `notes.html` (release notes) is comprised of six components:
   * `/templates/header.html`
   * `/templates/notes.html`
@@ -64,7 +79,7 @@ node $MAKE_SCRIPT
   * `/templates/changelogs/data.json`
   * `/templates/changelogs/releases/%MONTH_&_YEAR_CONTENT.html%`
   * `/templates/footer.html`
-### Build - Examples:
+### Compile - Examples:
 
 **How to update Header or Footer content**
 * Same setup, as described above.
--- make.js
+++ make.js
@@ -1,13 +1,31 @@
 // Import modules
-let HTTP = require("http");
-    URL = require("url"),
-    PATH = require("path"),
-    FS = require("fs")
-// Build Function
-let buildHTML = function() {
+let PATH = require("path");
+    FS = require("fs");
+    HTTP = require("http");
+    URL = require("url");
+    COMPILE_NOTES = true;
+    PORT = process.argv[2] || 80,
+    WEBPAGES = ["/404", "/notes", "/news", "/features", "/faq", "/community", "/download", "/privacy"];
+    MIME_TYPES = {
+      "html": "text/html",
+      "css": "text/css",
+      "js": "text/javascript",
+      "ico": "image/vnd.microsoft.icon",
+      "svg": "image/svg+xml",
+      "png": "image/png",
+      "jpeg": "image/jpeg",
+      "jpg": "image/jpeg",
+      "gif": "image/gif",
+      "webp": "image/webp",
+      "mp4": "video/mp4",
+      "webm": "video/webm",
+      "json": "application/json"
+    };
+// Function - Compile Notes
+function compileHTML() {
   FS.readdir("templates/", (err, file) => {
     file.forEach(file => {
-      if (PATH.extname(file) == ".html") {
+      if (PATH.extname(file) === ".html") {
         if (PATH.basename(file) !== "navbar.html" && PATH.basename(file) !== "footer.html") {
           let n = FS.readFileSync("templates/navbar.html", "utf8");
           let f = FS.readFileSync("templates/footer.html", "utf8");
@@ -15,26 +33,41 @@ let buildHTML = function() {
             let m = FS.readFileSync("templates/" + file, "utf8");
             FS.writeFileSync(file, n + "\n" + m + "\n" + f);
           } else if (PATH.basename(file) === "notes.html") {
-            let notes = FS.readFileSync("templates/notes.html", "utf8");
-            let content1 = FS.readFileSync("templates/changelogs/content.html", "utf8");
-            let DATA = FS.readFileSync("templates/changelogs/data.json");
-            let f = FS.readFileSync("templates/footer.html", "utf8");
-            for (i=1; i<=Object.keys(JSON.parse(DATA)).length; i++) {
-              let changelog = FS.readFileSync("templates/changelogs/releases/" + JSON.parse(DATA)[i].date + ".html", "utf8");
-              let content2 = content1.replace('<span></span>', '<span>' + JSON.parse(DATA)[i].title + '</span>');
-              let content3 = content2.replace('src=""', 'src="/img/' + JSON.parse(DATA)[i].image + '"');
-              let content4 = content3.replace('id="title" href="#title"', 'id="' + JSON.parse(DATA)[i].date + '-title"' + ' href="#' + JSON.parse(DATA)[i].date + '-title"');
-              let content5 = content4.replace('id="new" href="#new"', 'id="' + JSON.parse(DATA)[i].date + '-new"' + ' href="#' + JSON.parse(DATA)[i].date + '-new"');
-              let content6 = content5.replace('id="highlights" href="#highlights"', 'id="' + JSON.parse(DATA)[i].date + '-highlights"' + ' href="#' + JSON.parse(DATA)[i].date + '-highlights"');
-              let content7 = content6.replace('id="bug-fixes" href="#bug-fixes"', 'id="' + JSON.parse(DATA)[i].date + '-bug-fixes"' + ' href="#' + JSON.parse(DATA)[i].date + '-bug-fixes"');
-              let content8 = content7.replace('id="other-changes" href="#other-changes"', 'id="' + JSON.parse(DATA)[i].date + '-other-changes"' + ' href="#' + JSON.parse(DATA)[i].date + '-other-changes"');
-              let content9 = content8.replace('id="info_d"></div>', 'id="info_d">' + changelog.split("%INFO - START%").pop().split("%INFO - END%")[0] + '</div>');
-              let content10 = content9.replace('id="highlights_p"></p>', 'id="highlights_p">' + changelog.split("%HIGHLIGHTS - START%").pop().split("%HIGHLIGHTS - END%")[0] + '</p>');
-              let content11 = content10.replace('id="new_p"></p>', 'id="new_p">' + changelog.split("%NEW - START%").pop().split("%NEW - END%")[0] + '</p>');
-              let content12 = content11.replace('id="bug_fixes_p"></p>', 'id="bug_fixes_p">' + changelog.split("%BUG FIXES - START%").pop().split("%BUG FIXES - END%")[0] + '</p>');
-              let content13 = content12.replace('id="other_changes_p"></p>', 'id="other_changes_p">' + changelog.split("%OTHER CHANGES - START%").pop().split("%OTHER CHANGES - END%")[0] + '</p>');
-              notes = notes.replace("%c" + i + "%", content13);
-              FS.writeFileSync(file, n + "\n" + notes + "\n" + f);
+            if (COMPILE_NOTES === true) {
+              let notes = FS.readFileSync("templates/notes.html", "utf8");
+              let content1 = FS.readFileSync("templates/changelogs/content.html", "utf8");
+              let DATA = FS.readFileSync("templates/changelogs/data.json");
+              for (i=1; i<=Object.keys(JSON.parse(DATA)).length; i++) {
+                // Get - Changelogs
+                let changelog = FS.readFileSync("templates/changelogs/releases/" + JSON.parse(DATA)[i].date + ".html", "utf8");
+                // Set - Title (Text)
+                let content2 = content1.replace('<span></span>', '<span>' + JSON.parse(DATA)[i].title + '</span>');
+                // Set - Title (Image)
+                let content3 = content2.replace('src=""', 'src="/img/' + JSON.parse(DATA)[i].image + '"');
+                // Set - Anchors + Link (Title + New + Highlights + Bug-Fixes + Other-Changes)
+                let content4 = content3.replace('id="title" href="#title"', 'id="' + JSON.parse(DATA)[i].date + '-title"' + ' href="#' + JSON.parse(DATA)[i].date + '-title"');
+                let content5 = content4.replace('id="new" href="#new"', 'id="' + JSON.parse(DATA)[i].date + '-new"' + ' href="#' + JSON.parse(DATA)[i].date + '-new"');
+                let content6 = content5.replace('id="highlights" href="#highlights"', 'id="' + JSON.parse(DATA)[i].date + '-highlights"' + ' href="#' + JSON.parse(DATA)[i].date + '-highlights"');
+                let content7 = content6.replace('id="bug-fixes" href="#bug-fixes"', 'id="' + JSON.parse(DATA)[i].date + '-bug-fixes"' + ' href="#' + JSON.parse(DATA)[i].date + '-bug-fixes"');
+                let content8 = content7.replace('id="other-changes" href="#other-changes"', 'id="' + JSON.parse(DATA)[i].date + '-other-changes"' + ' href="#' + JSON.parse(DATA)[i].date + '-other-changes"');
+                // Set - Anchors (Download + Manual + Help)
+                let content9 = content8.replace('id="download"', 'id="' + JSON.parse(DATA)[i].date + '-download"');
+                let content10 = content9.replace('id="manual"', 'id="' + JSON.parse(DATA)[i].date + '-manual"');
+                let content11 = content10.replace('id="help"', 'id="' + JSON.parse(DATA)[i].date + '-help"');
+                // Set Links - (Download + Manual + Help)
+                let content12 = content11.replace('href="#download"', 'href="#' + JSON.parse(DATA)[i].date + '-download"');
+                let content13 = content12.replace('href="#manual"', 'href="#' + JSON.parse(DATA)[i].date + '-manual"');
+                let content14 = content13.replace('href="#help"', 'href="#' + JSON.parse(DATA)[i].date + '-help"');
+                // Set - Changelogs
+                let content15 = content14.replace('info_d"></div>', JSON.parse(DATA)[i].date + '-info">' + changelog.split("%INFO - START%").pop().split("%INFO - END%")[0] + '                </div>');
+                let content16 = content15.replace('id="highlights_d"></div>', 'id="' + JSON.parse(DATA)[i].date + '-highlights_d">' + changelog.split("%HIGHLIGHTS - START%").pop().split("%HIGHLIGHTS - END%")[0] + '                </div>');
+                let content17 = content16.replace('id="new_d"></div>', 'id="' + JSON.parse(DATA)[i].date + '-new_d">' + changelog.split("%NEW - START%").pop().split("%NEW - END%")[0] + '                </div>');
+                let content18 = content17.replace('id="bug_fixes_d"></div>', 'id="' + JSON.parse(DATA)[i].date + '-bug_fixes_d">' + changelog.split("%BUG FIXES - START%").pop().split("%BUG FIXES - END%")[0] + '                </div>');
+                let content19 = content18.replace('id="other_changes_d"></div>', 'id="' + JSON.parse(DATA)[i].date + '-other_changes_d">' + changelog.split("%OTHER CHANGES - START%").pop().split("%OTHER CHANGES - END%")[0] + '                </div>');
+                // Write - Content
+                notes = notes.replace("%c" + i + "%", content19);
+                FS.writeFileSync(file, n + "\n" + notes + "\n" + f);
+              }
             }
           }
         }
@@ -42,37 +75,19 @@ let buildHTML = function() {
     });
   });
 };
-// Init Build
-buildHTML();
-// Re-build On Changes
+// Compile - Init
+compileHTML();
+// Compile - On Detected Changes
 FS.watch("templates/", {recursive: true}, e => {
-  buildHTML();
+  compileHTML();
 });
-// Server Variables
-let PORT = process.argv[2] || 80,
-WEBPAGES = ["/404", "/notes", "/news", "/features", "/faq", "/community", "/download"]
-MIME_TYPES = {
-  "html": "text/html",
-  "css": "text/css",
-  "js": "text/javascript",
-  "ico": "image/vnd.microsoft.icon",
-  "svg": "image/svg+xml",
-  "png": "image/png",
-  "jpeg": "image/jpeg",
-  "jpg": "image/jpeg",
-  "gif": "image/gif",
-  "webp": "image/webp",
-  "mp4": "video/mp4",
-  "webm": "video/webm",
-  "json": "application/json"
-};
 // Create Server
 HTTP.createServer(function(req, res) {
   // Variables
   let URI = URL.parse(req.url).pathname, 
       fileName = PATH.join(process.cwd(), URI);
   // 302 - Redirect Valid URLs
-  if (URI == "" || URI == "/") {
+  if (URI === "" || URI === "/") {
     fileName = "index.html";
   } else if (WEBPAGES.includes(URI)) {
     fileName += ".html";
