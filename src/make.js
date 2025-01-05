abf4e636e3afce594dca473469286c8ac9b84399
luboslenco <lubos.lenco@gmail.com>
2025-01-05 13:10:31 +0100

    Sync

---
+++ src/make.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let header = fs.readFileSync('header.html', 'utf8');
+let cover = fs.readFileSync('cover.html', 'utf8');
+let footer = fs.readFileSync('footer.html', 'utf8');
+
+let write_html = function(fileName, extra = '') {
+	let html = fs.readFileSync(fileName, 'utf8');
+	fs.writeFileSync('../' + fileName, header + cover + html + extra + footer);
+}
+
+write_html('index.html');
+write_html('privacy.html');
+write_html('signin.html');
+write_html('signup.html');
+write_html('account.html');
+write_html('server.html');
